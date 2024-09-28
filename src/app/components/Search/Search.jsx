953d411580cd2f9ee5fd6de4d5f53e156ca7499c
bezawitassaye <beza1221love@gmail.com>
2024-09-28 09:45:10 -0400

    complete search componet functionalty

--- src/app/components/Search/Search.jsx
+++ src/app/components/Search/Search.jsx
@@ -1,13 +1,26 @@
+"use client";
 import React from 'react'
 import styles from "./Search.module.css"
 import { MdSearch } from 'react-icons/md'
+import { usePathname,useRouter, useSearchParams } from 'next/navigation'
+
 
 const Search = ({placeholder}) => {
+
+  const searchParams = useSearchParams();
+  const {replace} = useRouter();
+  const pathname = usePathname()
+
+  const handleSearch=(e)=>{
+    const params = new URLSearchParams(searchParams);
+    params.set("q",e.target.value);
+    replace(`${pathname}?${params}`)
+  }
   return (
     <div className={styles.container}>
         <MdSearch/>
         <input type="text" placeholder={placeholder} 
-        className={styles.input}/>
+        className={styles.input} onChange={handleSearch}/>
       
     </div>
   )
