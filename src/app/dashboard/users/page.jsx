aa70355a0ed484ab85b9a20fa3e3b74f41d444e8
bezawitassaye <beza1221love@gmail.com>
2024-09-28 03:13:28 -0400

    complet user and pagination ui

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
-
+import styles from "./users.module.css"
+import Search from '@/app/components/Search/Search'
+import Link from 'next/link'
+import Image from 'next/image'
+import Pagination from '@/app/components/Pagination/Pagination'
 const page = () => {
   return (
-    <div>
-      users page
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a use ..."/>
+        <Link href="/dashboard/users/add">
+           <button className={styles.addButton}>Add New</button>
+        </Link>
+
+      </div>
+      <table className={styles.table}>
+          <thead>
+             <tr>
+               <td>Name</td>
+               <td>Email</td>
+               <td>Created At</td>
+               <td>Role</td>
+               <td>Status</td>
+               <td>Action</td>
+             </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                  src="/avatar.png"
+                  alt=''
+                  width={40}
+                  height={40}
+                  className={styles.userimage}
+                  />
+                  Bezaeit
+                </div>
+              </td>
+              <td>jhon@gmail.com</td>
+              <td>13.01.2022</td>
+              <td>Admin</td>
+              <td>active</td>
+              <td>
+                <div className={styles.buttons}>
+                <Link href="/">
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                </Link>
+                
+                <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                </button>
+                </div>
+                
+              </td>
+            </tr>
+          </tbody>
+      </table>
+      <Pagination/>
     </div>
   )
 }
